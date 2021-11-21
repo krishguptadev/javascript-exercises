5c27b92444e4c4a8f19b375d3f672f5cd81261ac
Krish Gupta <krishguptadev@outlook.com>
2021-11-21 07:04:30 +0000

    excersie 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
 
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        return 'ERROR'
+    }
+
+    if (min < 0 || max < 0) {
+        return 'ERROR'
+    }
+
+    const data = [min, max].sort((a, b) => a - b)
+
+    let length = 0;
+    let sum = 0;
+
+    while (length < data[1] - data[0] + 1) {
+        sum += data[0] + length
+
+        length++
+    }
+
+    return sum
 };
 
 // Do not edit below this line
