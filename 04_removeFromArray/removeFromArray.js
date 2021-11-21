c1540afac4f1ccb9a6b639be2205fdd17ef56a15
Krish Gupta <krishguptadev@outlook.com>
2021-11-21 07:04:21 +0000

    excersie 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const { promisify } = require("util");
 
+const removeFromArray = function (array, ...items) {
+  const forEach = promisify(items.forEach);
+
+  let itemLength = array.length;
+
+  while (itemLength--) {
+    let arrayLength = array.length;
+
+    while (arrayLength--) {
+      if (array[arrayLength] === items[itemLength]) {
+        array.splice(arrayLength, 1);
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
